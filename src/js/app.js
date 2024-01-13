6ef128380eaf356565e9a3f36f57aea1c02dadf8
Sevak Martirosyan RedmiBook <Sevak.martirosyan1@gmail.com>
2024-01-13 16:26:02 +1000

    add workspace

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+
+import * as flsFunctions from "./modules/functions.js";
+import * as travelline from "./modules/travelline.js"
+
+travelline.TraveLline();
+flsFunctions.isWebp();
+
+console.log("hello world!");
+
